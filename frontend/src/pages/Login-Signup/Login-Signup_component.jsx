8bb7a364383daca87df2b5358eaf79a4a5750dc6
HardikChoudhary2003 <hardikchoudhary703@gmail.com>
2025-03-11 05:28:01 +0530

    signup almost done

--- frontend/src/pages/Login-Signup/Login-Signup_component.jsx
+++ frontend/src/pages/Login-Signup/Login-Signup_component.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import BrandingHeader from "./branding_header.jsx"
 import AuthPage from "./AuthPage.jsx";
+import "./Login-Signup_component.css"
 
 export default function LScomponent () {
   return (
